9bcd98efe9a24dedafedaf7b877f3c042a3d8dbb
monish <monishsharma080@gmail.com>
2024-07-06 13:10:09 +0530

    logging the vlaues to understand the crash more correctly

--- backend/views/views.py
+++ backend/views/views.py
@@ -112,7 +112,7 @@ class get_all_the_projects_of_the_user(mixins.CreateModelMixin,generics.GenericA
             os.getenv('NEXT_BACKEND_URL') + f"/get_all_the_projects_of_the_user?userName={userName}",
             headers={'content-type': 'application/json'}
         )
-        print(f"\n\n output from the go lang-->>{w.content} \n\n ")
+        print(f"\n\n output from the go lang-->>{w.content} \n\n  values -->",response_in_json.get('values'))
         response_in_json = w.json()
         return Response({"message_for_the_user":response_in_json.get('message_for_the_user'),"status_code":response_in_json.get('status_code'),"values":response_in_json.get('values'), "User_Name":userName},status=status.HTTP_200_OK)
         
