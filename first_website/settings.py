2d25c31c02e22d5d4c8d7d733e70103084293153
Monish Sharma <107314033+MONISHSHARMA080@users.noreply.github.com>
2024-11-04 11:14:18 +0530

    Update settings.py for using turso

--- first_website/settings.py
+++ first_website/settings.py
@@ -157,14 +157,21 @@ WSGI_APPLICATION = 'first_website.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 #  ----------db for prod --------
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.getenv('DB_ENGINE'),
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#     }
+# }
+# ------- using turs0 in gcp -----
 DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+    "default": {
+        "ENGINE": os.getenv('DB_ENGINE'),
+        "NAME": os.getenv('DB_NAME'),,
     }
 }
 
